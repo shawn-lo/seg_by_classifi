b0a15eaf5d1b0dea224538b376743529dec7a278
Shawn Luo <shawn.luotianyu@gmail.com>
2015-12-05 10:06:55 -0500

    main

--- segmentation.py
+++ segmentation.py
@@ -4,26 +4,98 @@ import scipy
 from scipy import ndimage
 from pylab import *
 from plyfile import PlyData, PlyElement
-import knn as ks
 
-# read ply data
-plyData = PlyData.read('./Data/Cornell/office_data_ply/scene1.ply')
-dataset = plyData['vertex'][:]
-print(plyData)
-# get labels
-labels_name = np.loadtxt('./Data/Cornell/features/labels.txt',converters={0: lambda x: unicode(x, 'utf-8')}, dtype='U2')
-labels_num = labels_name.shape[0]
-labels = np.zeros(labels_num)
-# split into train data and test data
-n = 1
-for number in range(0, n):
-    np.random.shuffle(dataset)
-    train_data, test_data = np.split(dataset, 2)
-    ks.knn(5,train_data, test_data, labels)
+import NearestNeighbors as nn
+import parsing
+import analysis
 
-#    vertex = np.array(plydata['vertex'][:], dtype=[('x', 'f4'), ('y','f4'), ('z', 'f4'),
-#    ('red','u1'), ('green','u1'),('blue','u1'),
-#    ('cameraIndex','u1'),('distance','f4'),
-#    ('segment','u1'),('label','u1')])
-#el = PlyElement.describe(vertex, 'vertex')
-#PlyData([el]).write('test.ply')
+from sklearn.svm import SVC
+from sklearn import tree
+
+if __name__ == '__main__':
+    # load raw data
+    parser = parsing.parsing()
+    raw_data = parser.parse_ply_data('./Data/Cornell/features/office_data_nodefeats.txt')
+
+    # create analysis utils
+    analyser = analysis.analysis()
+
+#=========================================================
+# K-NN
+#=========================================================
+    # classify
+    kSet = [3]
+    experiment_number = 10
+    for k in kSet:
+        print('This is for k = ', k, '\n')
+        k_classifier = nn.nearestneighbors(k)
+
+        for i in range(0, experiment_number):
+            np.random.shuffle(raw_data)
+            # 50-50
+            raw_train_data, raw_test_data = np.split(raw_data, 2)
+            # Get train data and test data
+            train_data = parser.parse_feature_data(raw_train_data)
+            test_data = parser.parse_feature_data(raw_test_data)
+            # Get train label and test label
+            train_label = parser.parse_label(raw_train_data)
+            test_label = parser.parse_label(raw_test_data)
+
+            # reduce the classes number, reduced_classes {(class label, number)}
+            reduced_size = 2
+            reduced_classes = analyser.reduce_classes(parser.parse_label(raw_data), reduced_size)
+            #print(reduced_classes)
+            reduced_label = np.zeros(reduced_size)
+            for i in range(reduced_size):
+                reduced_label[i] = reduced_classes[i][0]
+
+            # Deal with data, make it fit for new class label.
+            reduced_train_data, reduced_train_label = parser.parse_reduced_data(train_data, train_label, reduced_label)
+            reduced_test_data, reduced_test_label = parser.parse_reduced_data(test_data, test_label, reduced_label)
+
+            #print(reduced_train_label)
+
+            acc = k_classifier.knn(reduced_train_data, reduced_test_data, reduced_train_label, reduced_test_label)
+            #print(acc)
+
+#========================================================
+# SVM
+#========================================================
+for i in range(0, experiment_number):
+            np.random.shuffle(raw_data)
+            # 50-50
+            raw_train_data, raw_test_data = np.split(raw_data, 2)
+            # Get train data and test data
+            train_data = parser.parse_feature_data(raw_train_data)
+            test_data = parser.parse_feature_data(raw_test_data)
+            # Get train label and test label
+            train_label = parser.parse_label(raw_train_data)
+            test_label = parser.parse_label(raw_test_data)
+
+            # reduce the classes number, reduced_classes {(class label, number)}
+            reduced_size = 5
+            reduced_classes = analyser.reduce_classes(train_label, reduced_size)
+            reduced_label = np.zeros(reduced_size)
+            for i in range(reduced_size):
+                reduced_label[i] = reduced_classes[i][0]
+
+            # Deal with data, make it fit for new class label.
+            reduced_train_data, reduced_train_label = parser.parse_reduced_data(train_data, train_label, reduced_label)
+            reduced_test_data, reduced_test_label = parser.parse_reduced_data(test_data, test_label, reduced_label)
+            print(len(reduced_train_data))
+            print(len(reduced_train_label))
+            #print(type(reduced_train_data), type(reduced_train_label))
+            clf = SVC()
+            #clf = tree.DecisionTreeClassifier()
+            clf.fit(np.asarray(reduced_train_data), np.asarray(reduced_train_label))
+            predict = clf.predict(np.asarray(reduced_test_data))
+            correct = 0
+            wrong = 0
+            for i in range(0, len(predict)):
+                if predict[i] == reduced_test_label[i]:
+                    correct += 1
+                else:
+                    wrong += 1
+            print(correct,' ', wrong)
+            print(correct/(correct+wrong))
+
